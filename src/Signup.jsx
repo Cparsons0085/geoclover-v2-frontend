28140536f8ff7bb556f3ca6926d7484140c50ee9
Cristy Parsons <cristylynn0920@gmail.com>
2025-06-10 20:44:59 -0500

    Add placeholder Signup page to fix Netlify deploy error

---
+++ src/Signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Signup() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Sign Up (Coming Soon)</h2>
+      <p>This feature is still under development.</p>
+    </div>
+  );
+}
